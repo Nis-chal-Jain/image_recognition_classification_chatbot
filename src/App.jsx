f58ff2f1b4d3b8eafe0a555d6e82d91f709a1eb7
Nischal Jain <contactmenischal@gmail.com>
2024-09-07 11:21:13 +0530

    Revert "landing page"
    This reverts commit abefd7bf13a650ec629cc3f9bf3530dbca75248e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,288 @@
-import React from "react";
-import { SparklesCore } from "../components/ui/sparkles";
-import { Link } from "react-router-dom";
+import "regenerator-runtime";
+import React, { useState, useEffect } from "react";
+import sampleImage from "./image.png";
+import { RxCrossCircled } from "react-icons/rx";
+import ReactMarkdown from "react-markdown";
+import { BounceLoader } from "react-spinners";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
 function App() {
+  const [image, setImage] = useState(null);
+  const [imageData, setImageData] = useState(null);
+  const [value, setValue] = useState("");
+  const [response, setResponse] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [usingvoice, setUsingvoice] = useState(false);
+
+  const { transcript, listening, resetTranscript } = useSpeechRecognition();
+
+  const uploadImage = async (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImageData(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+  const connecting = async () => {
+    try {
+      const res = await fetch("https://gemini-model-server.onrender.com/", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      const response = await res.json();
+      if (response.message === "connected") {
+        console.log("Connected to the Server");
+      }
+    } catch (e) {
+      console.log("Server Connection Failed");
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (image && image.size > 5 * 1024 * 1024) {
+      alert(`Image size too large. Maximum allowed size is ${5}MB.`);
+      return;
+    }
+    connecting();
+  }, [image]);
+
+  useEffect(() => {
+    setValue(transcript);
+  }, [transcript]);
+  useEffect(() => {
+    if (!listening && transcript) {
+      setValue(transcript);
+      analyzeImage();
+    }
+  }, [transcript, listening]);
+
+  const analyzeImage = async () => {
+    if (!image) {
+      setError("Image must be Selected First");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setResponse((prev) => [...prev, `Qes: ${value}`]);
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: value,
+          image: imageData,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const response = await fetch(
+        "https://gemini-model-server.onrender.com/gemini",
+        options
+      );
+      const data = await response.text();
+      if (usingvoice) {
+        const speechsyn = window.speechSynthesis;
+        const utter = new SpeechSynthesisUtterance(data);
+        speechsyn.speak(utter);
+        setUsingvoice(false);
+      }
+      setResponse((prev) => [...prev, `Ans: ${data}`]);
+      setValue("");
+      resetTranscript();
+    } catch (error) {
+      console.error(error);
+      setError("Something went Wrong!!!");
+    }
+    setLoading(false);
+  };
+
+  const clear = () => {
+    setImage(null);
+    setImageData(null);
+    setValue("");
+    setResponse("");
+    setError("");
+  };
+
+  // Function to handle Enter key press
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      analyzeImage();
+    }
+  };
+
   return (
-    <div>
-      <div className="h-[100vh] relative w-[100vw] bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-        <div className="w-full absolute inset-0 h-screen">
-          <SparklesCore
-            id="tsparticlesfullpage"
-            background="transparent"
-            minSize={0.6}
-            maxSize={1.4}
-            particleDensity={100}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
+    <>
+      <div className="app ">
+        <div className="text-center  top-0 bg-white rounded-b-md">
+          <h1 className="p-1 text-3xl font-bold font-sans-serif">
+            Conversational and Image Recognization ChatBot
+          </h1>
         </div>
-        <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
-          DeepVisionaries
-        </h1>
-        <div className="flex z-40 flex-col sm:flex-row items-center gap-4 cursor-pointer mt-8">
-          <button className="px-4 py-2 cursor-pointer  bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-          <Link to="/image">Image Recognition Chatbot</Link> 
-          </button>
-          <button className="px-4 py-2  cursor-pointer text-white ">
-            AI Assistant Chatbot
-          </button>
-        </div>
+        <section className="flex flex-col mt-1 gap-1 md:flex-row md:flex-nowrap sm:flex-row sm:flex-wrap  lg:flex-row lg:flex-nowrap">
+          <div
+            className=" shadow-lg h-[80vh] lg:w-[42vw] lg:h-[92vh]  border-2  bg-white border-gray-600 flex flex-col 
+                gap-4 p-4 rounded-xl
+                border-[#00095]  hover:shadow-xl hover:border-gray-900 hover:border-3
+                transition ease-out sm:w-[90vw] sm:h-[92vh] md:w-[45vw] md:h-[92vh]"
+          >
+            <div className="max-h-[88%] min-h-[88%] flex self-center  min-w-full bg-white rounded-md">
+              <img
+                className="w-fit max-h-[78vh] m-auto rounded-md "
+                src={image ? URL.createObjectURL(image) : sampleImage}
+                alt="Image uploaded by the user"
+              />
+            </div>
+            <div className="border-black border-2 ">
+              <div className="rounded-md  imageInput text-center bg-white  ">
+                  <span className="p-1 pl-5 pr-6 h-full  border-2 rounded-md text-white cursor-pointer bg-[#121212]">
+                    Upload Image
+                  </span>
+                <input
+                  onChange={uploadImage}
+                  id="files"
+                  accept="image/*"
+                  type="file"
+                  className="w-[90%]"
+                  hidden
+                ></input>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className=" shadow-lg lg:w-[58vw] h-[92vh] border-2  bg-white border-black flex flex-col 
+            gap-4 p-4 rounded-xl   ease-out sm:w-[90vw] sm:h-[92vh] md:w-[45vw]"
+          >
+            <div className="max-h-[88%] min-h-[88%] w-full overflow-auto rounded-md relative">
+              {loading ? (
+                <BounceLoader
+                  color="#121212"
+                  speedMultiplier={1}
+                  size={70}
+                  className="absolute top-[46%] left-[46%]"
+                />
+              ) : (
+                response.map(function (data) {
+                  return (
+                    <div
+                      className={
+                        data ? "answer overflow-y-auto text-black" : ""
+                      }
+                      key={data.id}
+                    >
+                      <ReactMarkdown children={`${data ? data : ""}`} />
+                    </div>
+                  );
+                })
+              )}
+              {error && <p className="answer">{error}</p>}
+            </div>
+
+            <div className="text-[#121212] ">
+              <div className="relative ">
+                What do you want to know about the image?
+              </div>
+              {/* <div> */}
+              {/* <div className=" "> */}
+              <div className="input-container border-2 border-black">
+                <input
+                  value={value}
+                  placeholder="What is in the image..."
+                  onChange={(e) => {
+                    setValue(e.target.value);
+                  }}
+                  onKeyDown={handleKeyDown} // Add onKeyDown event listener
+                  className="max-w-[87.5%]"
+                />
+                <div className="relative w-[2.5%] cross bg-[#FFFF]">
+                  <span
+                    className="absolute top-[28%] right-[15%]"
+                    onClick={() => {
+                      setValue("");
+                    }}
+                  >
+                    <RxCrossCircled
+                      size={18}
+                      className="text-slate-400 hover:text-[#484747]"
+                    />
+                  </span>
+                </div>
+                {!error && (
+                  <>
+                    <button
+                      onClick={analyzeImage}
+                      className="bg-black hover:bg-gray-100 text-white font-semibold hover:text-black py-2 px-4 border-2 border-blue-500 hover:border-transparent rounded"
+                    >
+                      Send
+                    </button>
+                    <div
+                      onClick={() => {
+                        if (listening) {
+                          SpeechRecognition.stopListening();
+                        } else {
+                          setUsingvoice(true);
+                          SpeechRecognition.startListening();
+                        }
+                      }}
+                      className="my-auto cursor-pointer"
+                    >
+                      {listening ? <Start /> : <Mic />}
+                    </div>
+                  </>
+                )}
+                {error && (
+                  <button onClick={window.location.reload}>Reset</button>
+                )}
+              </div>
+              {/* </div> */}
+              {/* </div> */}
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
+  );
+}
+function Mic() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="33"
+      height="33"
+      fill="currentColor"
+      class="bi bi-mic-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z" />
+      <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5" />
+    </svg>
+  );
+}
+function Start() {
+  return (
+    <svg
+      width="33"
+      height="33"
+      viewBox="0 0 15 15"
+      fill="red"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 3C2 2.44772 2.44772 2 3 2H12C12.5523 2 13 2.44772 13 3V12C13 12.5523 12.5523 13 12 13H3C2.44772 13 2 12.5523 2 12V3ZM12 3H3V12H12V3Z"
+        fill="red"
+      ></path>
+    </svg>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,10 @@ import 'regenerator-runtime'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Image from './pages/Image.jsx'
 import './index.css'
-import "./pages/index.css"
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/image",
-    element: <Image/>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
